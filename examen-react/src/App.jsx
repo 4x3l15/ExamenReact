b78e2b77192854adc2342e73ef755bacf2c60ef8
4x3l15 <adramirez@escuelasproa.edu.ar>
2024-06-24 10:49:57 -0300

    hasta donde llegue

---
+++ examen-react/src/App.jsx
@@ -0,0 +1,25 @@
+import React, {useEffect ,useState} from 'react';
+import Navbar from './componentes/Navbar';
+import Container from './componentes/Container';
+import Footer from './componentes/Footer';
+import './App.css';
+
+function App() {
+const produts = [produts ,setProducts] , useState(produts);
+const category = [category,setCategory], useState(category);
+
+useEffect (() => { 
+  fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(data=>setProducts(data))
+}, [category]);
+  return (
+    <div className="App">
+      <Navbar produts ={produts} />
+      <Container  produts={produts} category={category} />
+      <Footer category={category} />
+    </div>
+  );
+}
+
+export default App;
---
+++ examen-react/src/componentes/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Footer from "./Footer.css";
+
+const Footer () => {
+
+}
+
+export default Footer;
